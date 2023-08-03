b1ffbaa42cce62648b4e418e189ebf919829a1cd
João Neves <joao95neves@gmail.com>
2023-08-03 11:40:45 +0100

    feat(NevesCS.AspNetCore.Extensions): added `HttpResponseExtensions.ReadContentAs<T>()`

---
+++ src/NevesCS.AspNetCore/Extensions/HttpResponseExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+
+namespace NevesCS.AspNetCore.Extensions
+{
+    public static class HttpResponseExtensions
+    {
+        /// <summary>
+        /// Deserializes <see cref="HttpResponseMessage.Content"/> into an object. <br />
+        /// Throws if <see cref="HttpResponseMessage.IsSuccessStatusCode"/> is <see cref="false"/>.
+        ///
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="response"></param>
+        /// <returns></returns>
+        /// <exception cref="HttpRequestException"></exception>
+        public static async Task<T?> ReadContentAs<T>(this HttpResponseMessage response, CancellationToken cancellationToken = default)
+        {
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new HttpRequestException(
+                    response.ReasonPhrase,
+                    null,
+                    response.StatusCode);
+            }
+
+            var dataAsString = await response.Content.ReadAsStringAsync(cancellationToken).ConfigureAwait(false);
+
+            return JsonConvert.DeserializeObject<T>(dataAsString);
+        }
+    }
+}
