88f5d81ba04765024ea45d7e7bb3cc6075afaa82
João Neves <joao95neves@gmail.com>
2023-08-03 11:05:19 +0100

    feat(Static): added Guid.IsNullOrEmpty()

---
+++ src/NevesCS.Static/Extensions/GuidExtensions.cs
@@ -0,0 +1,11 @@
+﻿
+namespace NevesCS.Static.Extensions
+{
+    public static class GuidExtensions
+    {
+        public static bool IsNullOrEmpty(this Guid? guid)
+        {
+            return guid == null || guid == Guid.Empty;
+        }
+    }
+}
---
+++ src/NevesCS.Tests/Static/Extensions/GuidExtensionsTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+
+using NevesCS.Static.Extensions;
+
+namespace NevesCS.Tests.Static.Extensions
+{
+    public class GuidExtensionsTests
+    {
+        [Fact]
+        public void IsNullOrEmpty_HasNoNullReference()
+        {
+            Guid? guid1 = Guid.Empty;
+            Guid? guid2 = null;
+            Guid? guid3 = Guid.NewGuid();
+
+            guid1.IsNullOrEmpty().Should().Be(true);
+            guid2.IsNullOrEmpty().Should().Be(true);
+            guid3.IsNullOrEmpty().Should().Be(false);
+        }
+    }
+}
