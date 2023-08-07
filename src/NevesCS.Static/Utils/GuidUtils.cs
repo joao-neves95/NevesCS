ac24fb9f448c2aac54834f313c11666e77e8479c
João Neves <joao95neves@gmail.com>
2023-08-07 12:12:38 +0100

    rename(GuidUtils): `StringHashIntoGuid` -> `HashStringIntoGuid`

--- src/NevesCS.Static/Utils/GuidUtils.cs
+++ src/NevesCS.Static/Utils/GuidUtils.cs
@@ -9,7 +9,7 @@ namespace NevesCS.Static.Utils
         /// Creates a new <see cref="Guid"/> by computing the <see cref="MD5"/> hash of a <see cref="string"/>.
         ///
         /// </summary>
-        public static Guid StringHashIntoGuid(string name)
+        public static Guid HashStringIntoGuid(string name)
         {
             using var hasher = MD5.Create();
             var hashBytes = hasher.ComputeHash(Encoding.UTF8.GetBytes(name));
--- src/NevesCS.Tests/Static/Utils/GuidUtilsTests.cs
+++ src/NevesCS.Tests/Static/Utils/GuidUtilsTests.cs
@@ -12,7 +12,7 @@ namespace NevesCS.Tests.Static.Utils
             const string input = "Test_Name";
             const string expectedOutput = "7f63491e-3e66-6cc0-157d-a6599d7c65ec";
 
-            Guid result = GuidUtils.StringHashIntoGuid(input);
+            Guid result = GuidUtils.HashStringIntoGuid(input);
 
             result.ToString().Should().Be(expectedOutput);
         }
