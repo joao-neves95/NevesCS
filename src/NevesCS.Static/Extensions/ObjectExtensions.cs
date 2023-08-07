3a3d747e8bd79ccf8d78c18185e1d6245531e075
João Neves <joao95neves@gmail.com>
2023-08-07 12:10:45 +0100

    feat(Static.ObjectExtensions): added `.CallMethodOfPropertyDynamically()`

--- src/NevesCS.Static/Extensions/ObjectExtensions.cs
+++ src/NevesCS.Static/Extensions/ObjectExtensions.cs
@@ -13,6 +13,41 @@ namespace NevesCS.Static.Extensions
             return @object;
         }
 
+        /// <summary>
+        /// E.g.:
+        /// <code>
+        /// myClassInstance.CallMethodOfPropertyDynamically(
+        ///     nameof(MyClass.WithThisListProperty),
+        ///     nameof(MyClass.WithThisListProperty.AddRange),
+        ///     someListOfValuesToAdd);
+        /// </code>
+        /// </summary>
+        public static void CallMethodOfPropertyDynamically(
+            this object target,
+            string propertyName,
+            string methodName,
+            object value)
+        {
+            var propertyInfo = target
+                .GetType()
+                .GetProperty(propertyName, BindingFlags.Public | BindingFlags.Instance)
+                ?? throw new MissingFieldException(propertyName);
+
+            var methodInfo = propertyInfo
+                .PropertyType
+                .GetMethod(methodName, BindingFlags.Public | BindingFlags.Instance)
+                ?? throw new MissingMethodException(methodName);
+
+            var targetProperty = propertyInfo.GetValue(target);
+
+            if (!methodInfo.IsStatic && targetProperty == null)
+            {
+                throw new NullReferenceException(propertyName);
+            }
+
+            methodInfo.Invoke(targetProperty, new[] { value });
+        }
+
         public static string Serialize(this object @obj)
         {
             return Newtonsoft.Json.JsonConvert.SerializeObject(@obj);
--- src/NevesCS.Tests/Static/Extensions/ObjectExtensionsTests.cs
+++ src/NevesCS.Tests/Static/Extensions/ObjectExtensionsTests.cs
@@ -8,7 +8,7 @@ namespace NevesCS.Tests.Static.Extensions
     public class ObjectExtensionsTests
     {
         [Fact]
-        public void ThrowIfNull_ShouldThrow_IfNullClass()
+        public void ThrowIfNull_Should_Throw_IfNullClass()
         {
             MockClass mockClass = null!;
 
@@ -20,13 +20,78 @@ namespace NevesCS.Tests.Static.Extensions
         }
 
         [Fact]
-        public void ThrowIfNull_ShouldNotThrow_IfNotNullClass()
+        public void ThrowIfNull_Should_NotThrow_IfNotNullClass()
         {
             MockClass mockClass = new();
 
             var action = () => mockClass.ThrowIfNull();
 
             action.Should().NotThrow();
+        }
+
+        private IEnumerable<string> GetMockEnumerable() => new[] { 1, 2, 3 }.Select(x => x.ToString());
+
+        [Fact]
+        public void CallMethodOfPropertyDynamically_Should_CallAddRange()
+        {
+            var values = GetMockEnumerable();
+
+            var mockClass = (object)new MockClass() { UselessList = new List<string>() };
+
+            CallAddRangeOnTarget(mockClass, values);
+
+            var castResult = (MockClass)mockClass;
+            castResult.UselessList.Should().BeEquivalentTo(values);
+        }
+
+        [Fact]
+        public void CallMethodOfPropertyDynamically_Should_ThrowOnInvalidProperty()
+        {
+            var values = Enumerable.Empty<string>();
+
+            var mockClass = (object)new MockClass();
+
+            var action = () => mockClass.CallMethodOfPropertyDynamically(
+                "ThisPropertyDoesNotExist",
+                nameof(MockClass.UselessList.AddRange),
+                values);
+
+            action.Should().Throw<MissingFieldException>().WithMessage("ThisPropertyDoesNotExist");
+        }
+
+        [Fact]
+        public void CallMethodOfPropertyDynamically_Should_ThrowOnInvalidMethod()
+        {
+            var values = Enumerable.Empty<string>();
+
+            var mockClass = (object)new MockClass();
+
+            var action = () => mockClass.CallMethodOfPropertyDynamically(
+                nameof(MockClass.UselessList),
+                "ThisMethodDoesNotExist",
+                values);
+
+            action.Should().Throw<MissingMethodException>().WithMessage("ThisMethodDoesNotExist");
+        }
+
+        [Fact]
+        public void CallMethodOfPropertyDynamically_Should_ThrowOnNullProperty()
+        {
+            var values = Enumerable.Empty<string>();
+
+            var mockClass = (object)new MockClass() { UselessList = null! };
+
+            var action = () => CallAddRangeOnTarget(mockClass, values);
+
+            action.Should().Throw<NullReferenceException>().WithMessage(nameof(MockClass.UselessList));
+        }
+
+        private void CallAddRangeOnTarget(object targetClass, object values)
+        {
+            targetClass.CallMethodOfPropertyDynamically(
+                nameof(MockClass.UselessList),
+                nameof(MockClass.UselessList.AddRange),
+                values);
         }
     }
 }
