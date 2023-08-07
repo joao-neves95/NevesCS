33eace63383da1d18c5d5d0c2ed665c591116613
João Neves <joao95neves@gmail.com>
2023-08-07 12:14:13 +0100

    feat: added some small utils

---
+++ src/NevesCS.Static/Extensions/BooleanExtensions.cs
@@ -0,0 +1,11 @@
+﻿
+namespace NevesCS.Static.Extensions
+{
+    public static class BooleanExtensions
+    {
+        public static bool IsTrue(this bool? target)
+        {
+            return target == true;
+        }
+    }
+}
---
+++ src/NevesCS.Static/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,14 @@
+﻿
+namespace NevesCS.Static.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        public static void ForEach<T>(this IEnumerable<T> enumeration, Action<T> action)
+        {
+            foreach (T item in enumeration)
+            {
+                action(item);
+            }
+        }
+    }
+}
