aa7f1cab7428630e6e86d7fe94d74ab581fe0081
João Neves <joao95neves@gmail.com>
2023-10-26 12:45:06 +0100

    feat(Static): added calculation utils

---
+++ src/NevesCS.Static/Utils/CalculationUtils.cs
@@ -0,0 +1,22 @@
+﻿namespace NevesCS.Static.Utils
+{
+    public static class CalculationUtils
+    {
+        public static bool IsEven(int value)
+        {
+            return value % 2 == 0;
+        }
+
+        public static int Concat(int left, int right)
+        {
+            int rightLength = 10;
+
+            while (rightLength <= right)
+            {
+                rightLength *= 10;
+            }
+
+            return (left * rightLength) + right;
+        }
+    }
+}
