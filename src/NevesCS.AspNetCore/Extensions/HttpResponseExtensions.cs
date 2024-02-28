b8125ec6d3d31dd64d0916cffe0c4cda5b824b8e
João Neves <joao95neves@gmail.com>
2024-02-28 17:44:47 +0000

    feat(AspNetCore.HttpResponseExtensions): added a way to deserialize json http responses with a custom json parser

--- src/NevesCS.AspNetCore/Extensions/HttpResponseExtensions.cs
+++ src/NevesCS.AspNetCore/Extensions/HttpResponseExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Newtonsoft.Json;
+using NevesCS.AspNetCore.Abstractions.Interfaces;
+
+using Newtonsoft.Json;
 
 namespace NevesCS.AspNetCore.Extensions
 {
@@ -13,7 +15,45 @@ namespace NevesCS.AspNetCore.Extensions
         /// <param name="response"></param>
         /// <returns></returns>
         /// <exception cref="HttpRequestException"></exception>
-        public static async Task<T?> ReadContentAs<T>(this HttpResponseMessage response, CancellationToken cancellationToken = default)
+        public static async Task<T?> ReadContentAsAsync<T>(this HttpResponseMessage response, CancellationToken cancellationToken = default)
+        {
+            var dataAsString = await response.ReadContentAsStringAsync(cancellationToken);
+
+            if (dataAsString == null)
+            {
+                return default;
+            }
+
+            return JsonConvert.DeserializeObject<T>(dataAsString);
+        }
+
+        /// <summary>
+        /// Deserializes <see cref="HttpResponseMessage.Content"/> into an object. <br />
+        /// Throws if <see cref="HttpResponseMessage.IsSuccessStatusCode"/> is <see cref="false"/>.
+        ///
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="response"></param>
+        /// <param name="jsonClient"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        /// <exception cref="HttpRequestException"></exception>
+        public static async Task<T?> ReadContentAsAsync<T>(
+            this HttpResponseMessage response,
+            IJsonClientAdapter jsonClient,
+            CancellationToken cancellationToken = default)
+        {
+            var dataAsString = await response.ReadContentAsStringAsync(cancellationToken);
+
+            if (dataAsString == null)
+            {
+                return default;
+            }
+
+            return jsonClient.DeserializeObject<T>(dataAsString);
+        }
+
+        private static async Task<string?> ReadContentAsStringAsync(this HttpResponseMessage response, CancellationToken cancellationToken = default)
         {
             if (!response.IsSuccessStatusCode)
             {
@@ -23,9 +63,7 @@ namespace NevesCS.AspNetCore.Extensions
                     response.StatusCode);
             }
 
-            var dataAsString = await response.Content.ReadAsStringAsync(cancellationToken).ConfigureAwait(false);
-
-            return JsonConvert.DeserializeObject<T>(dataAsString);
+            return await response.Content.ReadAsStringAsync(cancellationToken).ConfigureAwait(false);
         }
     }
 }
