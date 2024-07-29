e7efe9c6eee88a62d2c89497d8f36bdabed6bb2e
João Neves <joao95neves@gmail.com>
2024-07-29 16:11:46 +0100

    feat(TypeOf): added `Date` related types

--- src/NevesCS.Static/Constants/TypeOf.cs
+++ src/NevesCS.Static/Constants/TypeOf.cs
@@ -10,5 +10,9 @@ namespace NevesCS.Static.Constants
         public static readonly Type Int = typeof(int);
         public static readonly Type Float = typeof(float);
         public static readonly Type Decimal = typeof(decimal);
+
+        public static readonly Type DateTime = typeof(DateTime);
+        public static readonly Type DateTimeOffset = typeof(DateTimeOffset);
+        public static readonly Type DateOnly = typeof(DateOnly);
     }
 }
